7b06c4f0a9319699b39d81306b7a802d6e8739e0
Ibraheem aljarrah <ikaljarrah23@cit.just.edu.jo>
2026-07-09 14:31:33 +0300

    Implement Library class with book management methods

---
+++ Services/library.py
@@ -0,0 +1,34 @@
+class Library:
+    def __init__(self):
+        self.books=[]
+    def add_book(self,book):
+        self.books.append(book)
+    def remove_book(self,isbn):
+        idx=0
+        for b in self.books:
+            if b.isbn==isbn:
+                self.books.pop(idx)
+                return True
+            idx+=1
+        return False
+    def borrow_book(self):
+        pass
+    def return_book(self):
+        pass
+    def search_by_title(self,title):
+        books_with_same_title=[]
+        for b in self.books:
+            if b.title==title:
+                books_with_same_title.append(b.isbn)
+        return books_with_same_title
+    def search_by_author(self,author):
+        books_with_author_name=[]
+        for b in self.books:
+            if b.author==author:
+                books_with_author_name.append(b.title)
+        return books_with_author_name
+    def show_all_books(self):
+        for b in self.books:
+            print(b)
+    def show_borrowed_books(self):
+        pass
